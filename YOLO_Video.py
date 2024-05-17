3d35ab50efe6522a8428de67fedc857c65d89d77
Lokendra Singh Rao <73986555+lokendra-singh-rao@users.noreply.github.com>
2024-05-18 01:22:27 +0530

    Local to Git

---
+++ YOLO_Video.py
@@ -0,0 +1,50 @@
+from ultralytics import YOLO
+import cv2
+import math
+
+
+def video_detection(path_x):
+    video_capture = path_x
+    # Create a Webcam Object
+    cap = cv2.VideoCapture(video_capture)
+    # frame_width = int(cap.get(3))
+    # frame_height = int(cap.get(4))
+
+    model = YOLO("YOLO-Weights/latestAccident.pt")
+    classnames = ['Accident']
+    while True:
+        success, img = cap.read()
+        results = model(img, stream=True)
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                print(x1, y1, x2, y2)
+                conf = math.ceil((box.conf[0] * 100)) / 100
+                cls = int(box.cls[0])
+                try:
+                    class_name = classnames[cls]
+                    label = f'{class_name}{conf}'
+                    t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                    print(t_size)
+                    c2 = x1 + t_size[0], y1 - t_size[1] - 3
+                    if class_name == 'Dust Mask':
+                        color = (0, 204, 255)
+                    elif class_name == "Glove":
+                        color = (222, 82, 175)
+                    elif class_name == "Protective Helmet":
+                        color = (0, 149, 255)
+                    else:
+                        color = (85, 45, 255)
+                    if conf > 0.5:
+                        cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+                        cv2.rectangle(img, (x1, y1), c2, color, -1, cv2.LINE_AA)  # filled
+                        cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+                except IndexError:
+                    print(f"Invalid class index: {cls}")
+
+        yield img
+
+
+cv2.destroyAllWindows()
